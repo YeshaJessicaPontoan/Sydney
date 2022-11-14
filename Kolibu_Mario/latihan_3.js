678c0d1bfdf281f048b5dac41fa771ab54ba3549
Kolibu Mario Jonatan <90627806+MarioKolibu@users.noreply.github.com>
2022-11-14 08:23:38 +0800

    Create latihan_3.js

---
+++ Kolibu_Mario/latihan_3.js
@@ -0,0 +1,36 @@
+let person = [
+    {
+        Name: "John",
+        age: 35,
+    },
+    {
+        Name: "Bill",
+        age: 23,
+    },
+    {
+        Name: "Bob",
+        age: 17,
+    },
+    {
+        Name: "Jack",
+        age: 20,
+    },
+];
+
+
+let max = person.age[i];
+for(let i=0; i<person.age; i++){
+    if(max>person.age[i]) 
+    {
+        max=person.age[i];
+        console.log("Max Usia: ", max);
+    }
+};
+let min = person.age[i];
+for(let i=0; i<person.age; i++){
+    if(min>person.age[i]) 
+    {
+        min=person.age[i];
+        console.log("Min Usia: ", min);
+    }
+};
